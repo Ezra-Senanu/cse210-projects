d7a5a823b37168e27afd7fb420b3b220f0f1c208
Ezra Senanu <sen19001@byui.edu>
2024-02-28 13:11:08 -0700

    update

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,13 +3,16 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
+        //prompts user for their name 
         Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
 
         Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
-
+        
+        //displays users name
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
         
     }
