6cb2baa90a7b779a4a0bb39112bbcc5ede06e307
Ezra Senanu <sen19001@byui.edu>
2024-03-04 11:15:11 -0700

    update

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+       Job job1 = new Job();
+        job1._jobTitle = "Missionary";
+        job1._company = "LDS Church";
+        job1._startYear = 2019;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Tutor";
+        job2._company = "BYU-I";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
+
+        Job job3 = new Job();
+        job3._jobTitle = "Accounts-Specialist";
+        job3._company = "BYU-I";
+        job3._startYear = 2023;
+        job3._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Ezra Senanu";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job3);
+
+        myResume.Display();
+
+         
     }
 }
