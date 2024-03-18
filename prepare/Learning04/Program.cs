a1083d6cd1215add3f5a0d1c1c9e6073f68dc704
EZRA <cyrilsena3@outlook.com>
2024-03-18 14:23:17 -0600

    update commit

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        
+        Assignment a1 = new Assignment("Cyril Kofi", "Discrete Math");
+        Console.WriteLine(a1.GetSummary());
+
+    
+        MathAssignment a2 = new MathAssignment("Albert Quansah", "Permutations", "6.5", "9-18");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Florence Nightingale", "Passwords", "How many passwords can be generated?");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
